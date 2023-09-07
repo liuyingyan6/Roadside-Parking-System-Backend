7f90b68c33d4acc46eb29d49166e9163f06f4d1d
useryangtong <1360589713@qq.com>
2023-09-07 21:23:36 +0800

    9.7/21:23

--- parkingsys-operator/src/main/java/com/woniuxy/operator/controller/CarController.java
+++ parkingsys-operator/src/main/java/com/woniuxy/operator/controller/CarController.java
@@ -5,7 +5,9 @@ import com.alibaba.excel.EasyExcel;
 import com.alibaba.excel.support.ExcelTypeEnum;
 import com.github.pagehelper.PageHelper;
 import com.github.pagehelper.PageInfo;
+import com.woniuxy.operator.entity.Car;
 import com.woniuxy.operator.pojos.ResponseResult;
+import com.woniuxy.operator.vo.CarOrderVO;
 import com.woniuxy.operator.vo.CarVO;
 import lombok.SneakyThrows;
 import org.springframework.web.bind.annotation.*;
@@ -71,40 +73,8 @@ public class CarController {
                 .replaceAll("\\+", "%20");
         response.setHeader("Content-disposition",
                 "attachment;filename*=utf-8''" + fileName + ".xlsx");
-        PageHelper.startPage(pageNum,pageSize);
-        List<CarVO> list = carServiceImpl.getAll(key);
-        PageInfo<CarVO> pageInfo = new PageInfo<>(list);
-        return ResponseResult.ok(pageInfo);
     }
 
-    @PostMapping("/delete")
-    public ResponseResult geleteCar(String carNumber,Integer userId){
-
-        return null;
-    }
-
-    @SneakyThrows
-    @GetMapping("/exportExcel")//下载 导出
-    public void exportCarList(HttpServletResponse response){
-        setExcelRespProp(response,"车辆管理表");
-        List<CarVO> carVOList = carServiceImpl.getAll(null);
-        EasyExcel.write(response.getOutputStream())
-                .head(CarVO.class)
-                .excelType(ExcelTypeEnum.XLSX)
-                .sheet("停车系统车辆管理列表")
-                .doWrite(carVOList);
-    }
-
-    private void setExcelRespProp(HttpServletResponse response, String rawFileName) throws UnsupportedEncodingException {
-        //contenxt-type=text/html;charset=utf-8
-        //contenxt-type=application/json
-        response.setContentType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
-        response.setCharacterEncoding("utf-8");
-        String fileName = URLEncoder.encode(rawFileName, "UTF-8")
-                .replaceAll("\\+", "%20");
-        response.setHeader("Content-disposition",
-                "attachment;filename*=utf-8''" + fileName + ".xlsx");
-    }
 
     @GetMapping("/carOrderList")
     public ResponseResult getCarOrderList(Integer pageSize,Integer pageNum,String carNumber){
